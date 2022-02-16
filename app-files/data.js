94b1b6383328f9dd26dcadc432573e20ecec7ab6
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-02-16 08:25:52 -0500

    init

---
+++ app-files/data.js
@@ -0,0 +1,188 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5664,
+      "initialViewParameters": {
+        "yaw": 0.2823284670420154,
+        "pitch": 0.47242371384823656,
+        "fov": 1.202509770731038
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4774888662170964,
+          "pitch": 0.06803669611437435,
+          "rotation": 0,
+          "target": "1-area-2"
+        },
+        {
+          "yaw": -1.9297595312413147,
+          "pitch": 0.08248079962589472,
+          "rotation": 0,
+          "target": "2-area-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7160695205180332,
+          "pitch": 0.531775530573837,
+          "title": "SB-5",
+          "text": "Sediment Basin 5"
+        },
+        {
+          "yaw": -2.0569031505637696,
+          "pitch": 0.22006627518529065,
+          "title": "SB-4",
+          "text": "Sediment Basin 4"
+        }
+      ]
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5664,
+      "initialViewParameters": {
+        "yaw": -0.1892991177789476,
+        "pitch": 0.316678514262275,
+        "fov": 0.9865224412907415
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8671306891856077,
+          "pitch": 0.01259026798192231,
+          "rotation": 0,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7395442438431878,
+          "pitch": 0.1982951874997827,
+          "title": "SB-5",
+          "text": "Sediment Basin 5"
+        }
+      ]
+    },
+    {
+      "id": "2-area-3",
+      "name": "Area 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5568,
+      "initialViewParameters": {
+        "yaw": 0.02957199801840993,
+        "pitch": 0.468740906322779,
+        "fov": 1.252282695619243
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4857472289365745,
+          "pitch": -0.01647096421291039,
+          "rotation": 0,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2519585177309516,
+          "pitch": 1.0978572938609439,
+          "title": "SB-4",
+          "text": "Sediment Basin 4"
+        },
+        {
+          "yaw": 0.1330710002211415,
+          "pitch": 0.25820681311595095,
+          "title": "SB-5",
+          "text": "Sediment Basin 5"
+        }
+      ]
+    }
+  ],
+  "name": "Paw Paw 2 South 2-14-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
